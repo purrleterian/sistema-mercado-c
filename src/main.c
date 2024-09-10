2459ebe7029beaa20d291bd959a29ccc125a2401
unknown <jcno307@gmail.com>
2024-09-10 11:31:04 -0300

    Adicionei func. info de produtos com formatacao pra descontos

--- src/main.c
+++ src/main.c
@@ -10,12 +10,21 @@ typedef struct {
 
 } Produto;
 
+void linhaDivi(char c, int n);
 void banner(char p[]);
 void infoProduto(Produto p);
 
 int main() {
-    banner("Hello");
+    Produto arroz = {247, "Arroizin", 19.40, 0.5};
+    infoProduto(arroz);
     return 0;
+}
+
+void linhaDivi(char c, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%c", c);
+    }
+    printf("\n");
 }
 
 void banner(char p[]) {
@@ -30,6 +39,19 @@ void banner(char p[]) {
     for (int i = 0; i < tamanhoP + 4; i++) {
         printf("=");
     }
+    printf("\n");
 }
 
-// void infoProduto(Produto p) {}
+void infoProduto(Produto p) {
+    banner(p.nome);
+
+    printf("- ID: %d\n", p.id);
+    if (p.desconto > 0) {
+        printf("- Preco: %.2f (%.2f) // %.0f%% de desconto",
+               p.preco - (p.preco * p.desconto), p.preco, p.desconto * 100);
+    } else {
+        printf("- Preco: %.2f", p.preco);
+    }
+    printf("\n");
+    linhaDivi('-', 15);
+}
