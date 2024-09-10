d1f8e26683266617b14b04905506cd692c10926c
unknown <jcno307@gmail.com>
2024-09-10 11:08:06 -0300

    Adicionei func. banner pra decorar texto

--- src/main.c
+++ src/main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int id;
+    char nome[30];
+    double preco;
+    float desconto;
+
+} Produto;
+
+void banner(char p[]);
+void infoProduto(Produto p);
 
 int main() {
-    printf("Hello, World");
+    banner("Hello");
     return 0;
 }
+
+void banner(char p[]) {
+    size_t tamanhoP = strlen(p);
+    for (int i = 0; i < tamanhoP + 4; i++) {
+        printf("=");
+    }
+    printf("\n");
+    printf("= %s =", p);
+    printf("\n");
+
+    for (int i = 0; i < tamanhoP + 4; i++) {
+        printf("=");
+    }
+}
+
+// void infoProduto(Produto p) {}
