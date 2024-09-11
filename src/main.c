b45b4a2d4f3eab13407bd3717436d6cef682bbfc
unknown <jcno307@gmail.com>
2024-09-11 01:44:13 -0300

    Mudancas feitas para ajudar a diferenciar entre o registro e o carrinho

--- src/main.c
+++ src/main.c
@@ -13,35 +13,54 @@ typedef struct {
 typedef struct {
     Produto *produtos;
     int quantidade;
+    float total;
 
 } Carrinho;
+
+// Sistema = resgistra os produtos
+// Carrinho = guarda os produtos ja registrados
+
+typedef struct {
+    Produto *registro;
+    int n;
+} Sistema;
 
 void linhaDivi(char c, int n);
 void banner(char p[]);
 void infoProduto(Produto p);
 
-void adicCarrinho(Produto p, Carrinho *c);
-void produtosNoCarrinho(Carrinho c);
+void registrarProduto(Produto p, Sistema *s);
+void mostrarRegistro(Sistema s);
+
+void adicCarrinho(int id, Sistema s, Carrinho *c);
+void listarCarrinho(Carrinho c);
 
 int main() {
     Carrinho carrinho;
+    Sistema sistema;
 
     carrinho.quantidade = 0;
-    carrinho.produtos = malloc(sizeof(Produto));
+    carrinho.produtos = NULL;
+
+    sistema.n = 0;
+    sistema.registro = NULL;
 
     Produto arroz = {1, "Arroizin", 17.40, 0.5};
     Produto paes = {2, "Naes", 9.90, 0};
     Produto suco = {3, "Suquin", 11.99, 0};
     Produto bolo = {4, "Bolo de Cenoura", 15.50, 0};
 
-    adicCarrinho(arroz, &carrinho);
-    adicCarrinho(paes, &carrinho);
-    adicCarrinho(suco, &carrinho);
-    adicCarrinho(bolo, &carrinho);
+    // 4 items registrados no sistema
+    registrarProduto(arroz, &sistema);
+    registrarProduto(paes, &sistema);
+    registrarProduto(suco, &sistema);
+    registrarProduto(bolo, &sistema);
 
-    produtosNoCarrinho(carrinho);
+    // 1 item adicionado ao carrinho
+    adicCarrinho(3, sistema, &carrinho);
+    listarCarrinho(carrinho);
 
-    // TODO: verificar o tamanho do ponteiro dos produtos com o debugger
+    free(sistema.registro);
     free(carrinho.produtos);
     return 0;
 }
@@ -82,23 +101,59 @@ void infoProduto(Produto p) {
     linhaDivi('-', 15);
 }
 
-void adicCarrinho(Produto p, Carrinho *c) {
-    c->quantidade++;
-    if (c->quantidade >= 1) {
-        Produto *temp = realloc(c->produtos, c->quantidade * sizeof(Produto));
+void registrarProduto(Produto p, Sistema *s) {
+    s->n++;
+    if (s->n >= 1) {
+        Produto *temp = realloc(s->registro, s->n * sizeof(Produto));
 
         if (temp == NULL) {
             printf("Erro ao realocar memoria");
             exit(1);
         }
 
-        c->produtos = temp;
+        s->registro = temp;
     }
 
-    c->produtos[c->quantidade - 1] = p;
+    s->registro[s->n - 1] = p;
 }
 
-void produtosNoCarrinho(Carrinho c) {
+void adicCarrinho(int id, Sistema s, Carrinho *c) {
+    // ao adicionar um produto do registro para o carrinho,
+    // percorrer o registro em busca do produto desejado. Caso o produto
+    // nao for encontrado, ele ainda nao foi registrado. Se encontrado,
+    // adicionar ao carrinho, perguntando a quantidade a ser adicionada.
+    int encontrado = 0;
+    for (int i = 0; i < s.n; i++) {
+        if (id == s.registro[i].id) {
+            c->quantidade++;
+            if (c->quantidade >= 1) {
+                Produto *temp =
+                    realloc(c->produtos, c->quantidade * sizeof(Produto));
+
+                if (temp == NULL) {
+                    printf("Erro ao realocar memoria");
+                    exit(1);
+                }
+
+                c->produtos = temp;
+            }
+
+            encontrado = 1;
+            c->produtos[c->quantidade - 1] = s.registro[i];
+        }
+    }
+    if (!encontrado) {
+        printf("ID do produto fornecido nao encontrado no registro do sistema");
+    }
+}
+
+void mostrarRegistro(Sistema s) {
+    for (int i = 0; i < s.n; i++) {
+        infoProduto(s.registro[i]);
+    }
+}
+
+void listarCarrinho(Carrinho c) {
     for (int i = 0; i < c.quantidade; i++) {
         infoProduto(c.produtos[i]);
     }
